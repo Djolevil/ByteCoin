6d91e385ab81307773c306d30fab091114a70274
Djordje Knezevic <starwarrior@mycity.rs>
2022-12-15 15:31:15 +0100

    Parse JSON data received from Coinbase

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Djordje Knezevic on 15.12.22..
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
